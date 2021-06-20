dd937df8c8d1dd69bbe179bd0489d1c15fde8e60
Sofia Zavala <szavala3@outlook.com>
2021-06-20 09:26:24 -0400

    use DOM API and JavaScript to alternate two pictures

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,1 +1,10 @@
+let myImage = document.querySelector('img');
 
+myImage.onclick = functin() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === '../images/red-spotted-newt.jpeg') {
+        myImage.setAttribute('src', '../images/fire-salamander.jpeg');
+    } else {
+        myImage.setAttribute('src', '../images/red-spotted-newt.jpeg');
+    }
+}
