9f72b503f676857f94ac93b8eddfc629cef822b6
Sofia Zavala <szavala3@outlook.com>
2021-06-20 09:42:18 -0400

    use WebStorage API to make custom welcome message

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -8,3 +8,26 @@ myImage.onclick = functin() {
         myImage.setAttribute('src', '../images/red-spotted-newt.jpeg');
     }
 }
+
+let myButton = document.querySelector('button')
+let myHeading = document.querySelector('h1')
+
+function setUserName() {
+    /* prompt() is like alert() but it accepts user input and stores it as a variable*/
+    let myName = prompt('Please enter your name.');
+    /* localStorage is an API that stores data in the browser.*/
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'The newt welcomes you, ' + myName;
+}
+
+/* use ! to check if 'name' exists in localStorage.*/
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'The salamander welcomes you, ' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName()
+}
